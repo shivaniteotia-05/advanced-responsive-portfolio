6bbb3b7e2863dca2ba0c7d73846c8547bf4089de
shivaniteotia-05 <shivaniteotia91@gmail.com>
2026-06-13 21:50:30 +0530

    Add files via upload

---
+++ advanced-responsive-portfolio/css/js/script.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        toggleBtn.textContent = "☀️";
+    }else{
+        toggleBtn.textContent = "🌙";
+    }
+});
